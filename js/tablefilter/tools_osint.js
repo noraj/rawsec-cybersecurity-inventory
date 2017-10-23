0cce1893751f29acf4db0409810272a0a979380e
Alexandre ZANNI <alexandre.zanni@engineer.com>
2017-10-23 19:39:37 +0200

    issue #24

---
+++ js/tablefilter/tools_osint.js
@@ -0,0 +1,25 @@
+var filtersConfig = Object.assign({}, baseConfig);
+jQuery.extend(filtersConfig, {
+    // Filter types
+    col_0: 'input',
+    col_1: 'none',
+    col_2: 'none',
+    col_3: 'input',
+    col_4: 'select',
+    col_5: 'select',
+    col_6: 'select',
+    // columns data types
+    col_types: [
+        'caseinsensitivestring',
+        'none',
+        'none',
+        'string',
+        'string',
+        'string',
+        'string',
+    ],
+    enable_empty_option: true,
+    enable_non_empty_option: true,
+});
+var tf = new TableFilter(document.querySelector('#tools-osint'), filtersConfig);
+tf.init();
