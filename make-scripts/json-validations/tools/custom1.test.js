e065e8a62780758a6bd5f031f6b8c2fd739af53e
Rohan Karan <rohankaran001@gmail.com>
2022-10-09 20:38:21 +0530

    json-validation 4 done

--- make-scripts/json-validations/tools/custom1.test.js
+++ make-scripts/json-validations/tools/custom1.test.js
@@ -3,7 +3,7 @@ const path = require("path");
 const fs = require("fs");
 const addFormats = require("ajv-formats")
 
-exclude = [
+include = [
   "plugins.json"
 ]
 const ajv = new Ajv({allErrors: true})
@@ -45,9 +45,12 @@ const custom1 = {
           },
           blackarch: {
             type: "string"
+          },
+          tool: {
+            type: "string"
           }
         },
-        required: ["name", "description", "price", "online"],
+        required: ["name", "description", "price", "tool", "online"],
         additionalProperties: false
       },
       minItems: 1
@@ -63,14 +66,15 @@ async function validator(path) {
   const dir = await fs.promises.opendir(path)
   let errors = []
   for await (const dirent of dir) {
-    if (exclude.includes(dirent.name)) continue;
-    const file = await fs.readFileSync(path +  "/" + dirent.name)
-      const targetJSON = JSON.parse(file.toString())
-      let valid = validate(targetJSON)
-      if (!valid) {
-        console.error("Error at " + dirent.name)
-        console.error(validate.errors)
-        errors.push(...validate.errors)
+    if (include.includes(dirent.name)){
+      const file = await fs.readFileSync(path +  "/" + dirent.name)
+        const targetJSON = JSON.parse(file.toString())
+        let valid = validate(targetJSON)
+        if (!valid) {
+          console.error("Error at " + dirent.name)
+          console.error(validate.errors)
+          errors.push(...validate.errors)
+        }
       }
     }
   return errors
