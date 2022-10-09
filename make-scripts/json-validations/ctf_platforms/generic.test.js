d87c7e79d45aca4c7e8ac92c95132d96a5266ff3
Rohan Karan <rohankaran001@gmail.com>
2022-10-09 20:38:21 +0530

    json-validation 4 done

--- make-scripts/json-validations/ctf_platforms/generic.test.js
+++ make-scripts/json-validations/ctf_platforms/generic.test.js
@@ -6,6 +6,8 @@ const addFormats = require("ajv-formats");
 const ajv = new Ajv({allErrors: true, removeAdditional: "all"})
 addFormats(ajv)
 
+exclude = []
+
 const genericMetaSchema = {
   type: "object",
   properties: {
@@ -53,6 +55,7 @@ async function validator(path) {
   const dir = await fs.promises.opendir(path)
   let errors = []
   for await (const dirent of dir) {
+    if (exclude.includes(dirent.name)) continue;
     const file = await fs.readFileSync(path +  "/" + dirent.name)
     const targetJSON = JSON.parse(file.toString())
     let valid = validate(targetJSON)
