04658dd5d1458bc05f748a03a5e2e8658087053f
noraj <alexandre.zanni@europe.com>
2024-07-23 23:26:32 +0200

    new analytics script

--- js/site.js
+++ js/site.js
@@ -15,6 +15,3 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
-
-// Counter Analytics
-if(!sessionStorage.getItem("_swa")&&document.referrer.indexOf(location.protocol+"//"+location.host)!== 0){fetch("https://counter.dev/track?"+new URLSearchParams({referrer:document.referrer,screen:screen.width+"x"+screen.height,user:"noraj",utcoffset:"1"}))};sessionStorage.setItem("_swa","1");
